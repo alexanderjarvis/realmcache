6889a11ee852d36e8a03b8244a886953f009b522
Alex Jarvis <alex@panaxiom.co.uk>
2015-05-10 17:39:26 +0100

    Initial commit

---
+++ RealmCache/RealmCache.h
@@ -0,0 +1,19 @@
+//
+//  RealmCache.h
+//  RealmCache
+//
+//  Created by Alex on 10/05/2015.
+//  Copyright (c) 2015 PANAXIOM Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmCache.
+FOUNDATION_EXPORT double RealmCacheVersionNumber;
+
+//! Project version string for RealmCache.
+FOUNDATION_EXPORT const unsigned char RealmCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmCache/PublicHeader.h>
+
+
---
+++ RealmCache/RealmCache.swift
@@ -0,0 +1,103 @@
+//
+//  RealmCache.swift
+//  RealmCache
+//
+//  Created by Alex on 10/05/2015.
+//  Copyright (c) 2015 PANAXIOM Ltd. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+public class RealmCache {
+
+    static let defaultCacheName = "cache"
+
+    let name: String
+    let realm: Realm
+
+    private static func cachesDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true) as! [String]
+        return paths[0]
+    }
+
+    init(name: String = defaultCacheName) {
+        self.name = name + ".realm"
+        self.realm = Realm(path: RealmCache.cachesDirectory() + name)
+    }
+
+    deinit {
+        realm.invalidate()
+    }
+
+    func objectForKey(key: String) -> AnyObject? {
+        if let cacheObject = realm.objectForPrimaryKey(CacheObject.self, key: key) {
+            if expired(cacheObject) {
+                removeObjectForKey(key)
+                return nil
+            } else if let object: AnyObject = NSKeyedUnarchiver.unarchiveObjectWithData(cacheObject.value) {
+                return object
+            }
+        }
+        return nil
+    }
+
+    func setObject(obj: NSSecureCoding, forKey key: String, expiresIn: NSTimeInterval = 0) {
+        realm.write { [unowned self] in
+            let cacheObject = CacheObject()
+            cacheObject.key = key
+            cacheObject.value = NSKeyedArchiver.archivedDataWithRootObject(obj)
+            cacheObject.created = NSDate().timeIntervalSince1970
+            cacheObject.expiresIn = expiresIn
+            self.realm.add(cacheObject, update: true)
+        }
+    }
+
+    func removeObjectForKey(key: String) {
+        if let cacheObject = realm.objectForPrimaryKey(CacheObject.self, key: key) {
+            realm.write { [unowned self] in
+                self.realm.delete(cacheObject)
+            }
+        }
+    }
+
+    func removeAllObjects() {
+        realm.write { [unowned self] in
+            self.realm.deleteAll()
+        }
+    }
+
+    private func expired(cacheObject: CacheObject) -> Bool {
+        if cacheObject.expiresIn != 0 {
+            let now = NSDate().timeIntervalSince1970
+            let expires = cacheObject.created + cacheObject.expiresIn
+            if now > expires {
+                return true
+            }
+        }
+        return false
+    }
+
+    func pruneExpired() {
+        let results = realm.objects(CacheObject).filter("expiresIn != 0")
+        for cacheObject in results {
+            if expired(cacheObject) {
+                removeObjectForKey(cacheObject.key)
+            }
+        }
+    }
+
+}
+
+class CacheObject: Object {
+
+    dynamic var key: String = ""
+    dynamic var value: NSData = NSData()
+    dynamic var created: NSTimeInterval = 0
+    dynamic var expiresIn: NSTimeInterval = 0
+
+    static override func primaryKey() -> String? {
+        return "key"
+    }
+
+}
